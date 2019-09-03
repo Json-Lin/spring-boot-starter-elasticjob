b385c342611d8a75c8e9f4bedb17ecc80beddebd
jiasonglin <447995687@qq.com>
2019-09-03 22:28:20 +0800

    update autoconfigura

--- src/main/java/com/cheese/elastic/autoconfigure/annotation/ElasticJobScheduler.java
+++ src/main/java/com/cheese/elastic/autoconfigure/annotation/ElasticJobScheduler.java
@@ -1,7 +1,5 @@
 package com.cheese.elastic.autoconfigure.annotation;
 
-import org.springframework.stereotype.Component;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -15,36 +13,42 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Component
 public @interface ElasticJobScheduler {
     /**
      * 作业名称
      */
     String jobName() default "";
+
     /**
      * 作业执行的cron表达式
      */
     String cron() default "";
+
     /**
      * 作业分片参数
      */
     String itemParameters() default "";
+
     /**
      * 作业描述
      */
     String desc() default "";
+
     /**
      * 作业分片总数
      */
     int shardingTotalCount() default 1;
+
     /**
      * 每次重启任务是否覆盖配置中心的配置信息
      */
     boolean overwrite() default false;
+
     /**
      * 作业错过执行时间是否立即触发一次
      */
     boolean misfire() default false;
+
     /**
      * 是否流式处理
      */
